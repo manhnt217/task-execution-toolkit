3aad1fb74c8a89851ebe531b34b5ac0d53e06574
Manh Nguyen <manhnguyen@fortna.com>
2023-09-10 16:26:53 +0700

    Load template class using reflection

--- src/main/java/io/github/manhnt217/task/task_executor/process/Template.java
+++ src/main/java/io/github/manhnt217/task/task_executor/process/Template.java
@@ -3,27 +3,37 @@ package io.github.manhnt217.task.task_executor.process;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.github.manhnt217.task.task_executor.Main;
-import io.github.manhnt217.task.task_executor.process.builtin.CurlTemplate;
-import io.github.manhnt217.task.task_executor.process.builtin.LogTemplate;
-import io.github.manhnt217.task.task_executor.process.builtin.SqlTemplate;
 import io.github.manhnt217.task.task_executor.task.TaskExecutionException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public abstract class Template<P, R> {
 
-	// TODO: Load process based on className (reflection) will be added later.
-	public static JsonNode run(String templateClassName, JsonNode input, LogHandler log) throws TemplateExecutionException {
-		switch (templateClassName) {
-			case "CurlTemplate":
-				return new CurlTemplate().run(input, log);
-			case "LogTemplate":
-				return new LogTemplate().run(input, log);
-			case "SqlTemplate":
-				return new SqlTemplate().run(input, log);
-			default:
-				throw new IllegalArgumentException("Template not found: " + templateClassName);
-		}
-	}
+	private static final String BUILTIN_TEMPLATE_PACKAGE = Template.class.getPackage().getName() + ".builtin.";
 
+	public static JsonNode run(String templateName, JsonNode input, LogHandler log) throws TemplateExecutionException {
+		try {
+			String className = BUILTIN_TEMPLATE_PACKAGE + templateName;
+			Class<?> clazz = Class.forName(className);
+			if (!Template.class.isAssignableFrom(clazz)) {
+				throw new TemplateExecutionException("Template class " + className + " does not extend " + Template.class.getName());
+			}
+
+			Method runMethod = clazz.getMethod("run", JsonNode.class, LogHandler.class);
+			Object templateInstance = clazz.newInstance();
+			Object result = runMethod.invoke(templateInstance, input, log);
+			return Main.om.valueToTree(result);
+		} catch (ClassNotFoundException e) {
+			throw new TemplateExecutionException("Could not find any builtin template with name: " + templateName);
+		} catch (NoSuchMethodException e) {
+			throw new TemplateExecutionException("Could not find method run(JsonNode, LogHandler) in template class: " + templateName);
+		} catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+			throw new TemplateExecutionException("Could not execute method run(JsonNode, LogHandler) in template class: " + templateName, e);
+        }
+    }
+
+	@SuppressWarnings("unused")
 	public final JsonNode run(JsonNode inputJS, LogHandler log) throws TemplateExecutionException {
 		P input;
 		try {
@@ -32,16 +42,14 @@ public abstract class Template<P, R> {
 			log.log(Severity.ERROR, "Cannot convert input");
 			throw new TaskExecutionException("Cannot convert input. Process stop", e);
 		}
-		R rs = null;
 		try {
-			rs = exec(input, log);
+			R rs = exec(input, log);
+			return Main.om.valueToTree(rs);
 		} catch (Exception e) {
 			throw new TemplateExecutionException(e);
 		}
-		return Main.om.valueToTree(rs);
 	}
 
-	// TODO: Use reflection to avoid implementing this method
 	protected abstract Class<? extends P> getInputClass();
 
 	public abstract R exec(P input, LogHandler log);
--- src/main/java/io/github/manhnt217/task/task_executor/process/TemplateExecutionException.java
+++ src/main/java/io/github/manhnt217/task/task_executor/process/TemplateExecutionException.java
@@ -2,5 +2,14 @@ package io.github.manhnt217.task.task_executor.process;
 
 public class TemplateExecutionException extends Exception {
 	public TemplateExecutionException(Exception e) {
+		super(e);
+	}
+
+	public TemplateExecutionException(String message) {
+		super(message);
+	}
+
+	public TemplateExecutionException(String message, Exception e) {
+		super(message, e);
 	}
 }
