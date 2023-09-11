28dd9ebcaf542c161855507bec95a8f42d8605c7
Manh Nguyen <manhnguyen@fortna.com>
2023-09-11 22:08:07 +0700

    Add a dummy dependency resolver

--- src/main/java/io/github/manhnt217/task/task_executor/executor/CompoundTaskExecutor.java
+++ src/main/java/io/github/manhnt217/task/task_executor/executor/CompoundTaskExecutor.java
@@ -10,9 +10,8 @@ import io.github.manhnt217.task.task_executor.task.TaskExecutionContext;
 import io.github.manhnt217.task.task_executor.task.TaskExecutionException;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static io.github.manhnt217.task.task_executor.Main.om;
@@ -29,9 +28,14 @@ public class CompoundTaskExecutor extends TaskExecutor {
 
         TaskExecutionContext context = new TaskExecutionContext();
 
-        context.saveInitialInput(input);
+        context.saveGlobalInput(input);
 
-        List<Task> executionOrder = resolveDependencies(compoundTask.getSubTasks());
+        List<Task> executionOrder = null;
+        try {
+            executionOrder = resolveDependencies(compoundTask.getSubTasks());
+        } catch (UnresolvableDependencyException e) {
+            throw new TaskExecutionException("Cannot execute task because of unresolvable dependencies", e);
+        }
         for (Task subTask : executionOrder) {
             executeTask(subTask, context);
         }
@@ -39,9 +43,23 @@ public class CompoundTaskExecutor extends TaskExecutor {
         return context.applyTransform(task.getOutputMappingExpression());
     }
 
-    private List<Task> resolveDependencies(Set<Task> subTasks) {
-        // TODO: dummy implementation
-        return subTasks.stream().sorted(Comparator.comparing(Task::getId)).collect(Collectors.toList());
+    private List<Task> resolveDependencies(Set<Task> tasks) throws UnresolvableDependencyException {
+        Map<String, Task> taskMap = tasks.stream().collect(Collectors.toMap(Task::getId, Function.identity()));
+        Set<DependentItem> dependentItems = tasks.stream()
+                .map(t -> new DependentItem(t.getId(), new HashSet<>(t.getDependencies())))
+                        .collect(Collectors.toSet());
+        DependencyResolver resolver = new DependencyResolver(dependentItems);
+        List<Task> result = new ArrayList<>();
+
+        for (DependentItem nextItem; (nextItem = resolver.next()) != null; ) {
+            if (nextItem == null) {
+                break;
+            } else {
+                result.add(taskMap.get(nextItem.getName()));
+            }
+        }
+
+        return result;
     }
 
     // TODO: Deal with TaskExecutionException
@@ -49,14 +67,14 @@ public class CompoundTaskExecutor extends TaskExecutor {
 
         JsonNode inputAfterTransform = extractInput(task, context);
 
-        context.saveInput(task, inputAfterTransform);
+        context.saveTaskInput(task, inputAfterTransform);
         log(task.getStartLogExpression(), context);
 
         TaskExecutor executor = getTaskExecutor(task);
         JsonNode output = executor.execute(task, inputAfterTransform);
         logs.addAll(executor.getLogs());
 
-        context.saveOutput(task, output);
+        context.saveTaskOutput(task, output);
         log(task.getEndLogExpression(), context);
     }
 
@@ -66,7 +84,7 @@ public class CompoundTaskExecutor extends TaskExecutor {
         } else if (Task.InputType.CONTEXT.equals(task.getInputType())) {
             return context.applyTransform(task.getInputMappingExpression());
         } else if (Task.InputType.PREVIOUS_TASK.equals(task.getInputType()) && task.getDependencies().size() == 1) {
-            return context.applyFromTaskOutput(task.getDependencies().get(0), task.getInputMappingExpression());
+            return context.applyFromTaskOutput(task.getDependencies().iterator().next(), task.getInputMappingExpression());
         } else {
             throw new TaskExecutionException("A task should depend on ONLY ONE task when its InputType = Previous Task");
         }
