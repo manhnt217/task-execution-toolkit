25fd28fc4e72005e9e01484cf6b3fe615d3695c9
Manh Nguyen <manhnguyen@fortna.com>
2023-10-15 18:14:36 +0700

    Introduce global properties: _PROPS_

--- src/main/java/io/github/manhnt217/task/task_executor/Main.java
+++ src/main/java/io/github/manhnt217/task/task_executor/Main.java
@@ -1,12 +1,8 @@
 package io.github.manhnt217.task.task_executor;
 
-import io.github.manhnt217.task.task_executor.task.TaskExecutionException;
-
-import java.io.IOException;
-
 public class Main {
 
-	public static void main(String[] args) throws IOException, TaskExecutionException {
+	public static void main(String[] args) {
 	}
 
 }
