30ff6fdc58616929d662fc1b0f55f0affa0afdb8
Manh Nguyen <manh.nguyen.217@gmail.com>
2024-12-29 14:38:45 +0700

    Add recursive task test

---
+++ sample-project/src/main/java/io/github/manhnt217/task/sample/SimpleTaskResolver.java
@@ -0,0 +1,28 @@
+package io.github.manhnt217.task.sample;
+
+import io.github.manhnt217.task.task_engine.task.Task;
+import io.github.manhnt217.task.task_engine.task.TaskResolver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author manhnguyen
+ */
+public class SimpleTaskResolver implements TaskResolver {
+    protected final Map<String, Task> tasks;
+
+    public SimpleTaskResolver() {
+        tasks = new HashMap<>();
+    }
+
+    @Override
+    public Task resolve(String name) {
+        return tasks.get(name);
+    }
+
+    @Override
+    public void register(Task task) {
+        tasks.put(task.getName(), task);
+    }
+}
